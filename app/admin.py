5df00906bc777f9389ca2c87f88363a79c222396
Andrei00001 <sherlat01@mail.ru>
2022-11-04 17:02:02 +0300

    add Enterprise Type

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from app.models import EnterpriseType
+
+register_admin = admin.site.register
+
+
+class EnterpriseTypeAdmin(admin.ModelAdmin):
+    model = EnterpriseType
+    ordering = ['id']
+    list_display = ['type', ]
+    fields = ['type', ]
+
+
+register_admin(EnterpriseType, EnterpriseTypeAdmin)
